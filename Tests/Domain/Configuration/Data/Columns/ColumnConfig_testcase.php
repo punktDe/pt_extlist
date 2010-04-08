c3a2783d516218e8173e1284fbfaef4d99a7e784
Daniel Lienert <lienert@punkt.de>
2010-04-08 16:34:42 +0200

    ADD: ColumnConfig & TestCase

---
+++ Tests/Domain/Configuration/Data/Columns/ColumnConfig_testcase.php
@@ -0,0 +1,74 @@
+<?php
+class Tx_PtExtlist_Domain_Configuration_Data_Fields_ColumnConfig_testcase extends Tx_Extbase_BaseTestcase {
+
+	/**
+	 * Holds a dummy configuration for a column config object
+	 * @var array
+	 */
+	protected $columnSettings = array();
+	
+	
+	/**
+	 * Holds an instance of column configuration object
+	 * @var Tx_PtExtlist_Domain_Configuration_Columns_ColumnConfig
+	 */
+	protected $columnConfig = null; 
+	
+	
+	/**
+	 * @return void
+	 * @author Daniel Lienert <lienert@punkt.de>
+	 */
+	public function setup() {
+		$this->columnSettings = array(
+		    'fieldIdentifier' => 'testField',
+		    'columnIdentifier' => 'testColumn',
+		    'label' => 'Test Column'
+		);
+		
+		$this->columnConfig = new Tx_PtExtlist_Domain_Configuration_Columns_ColumnConfig($this->columnSettings);
+	}
+	
+	
+	
+	public function testGetFieldIdentifier() {
+		$this->assertEquals($this->columnConfig->getFieldIdentifier(), $this->columnSettings['fieldIdentifier']);
+	}
+	
+	
+	
+	public function testGetColumnIdentifier() {
+		$this->assertEquals($this->columnConfig->getColumnIdentifier(), $this->columnSettings['columnIdentifier']);
+	}
+	
+	public function testGetLabelWhenLabelGiven() {
+		$this->assertEquals($this->columnConfig->getLabel(), $this->columnSettings['label']);
+	}
+	
+	public function testGetLabelWhenLabelNotGiven() {
+		unset($this->columnSettings['label']);
+		$this->columnConfig = new Tx_PtExtlist_Domain_Configuration_Columns_ColumnConfig($this->columnSettings);
+		$this->assertEquals($this->columnConfig->getLabel(), $this->columnSettings['columnIdentifier']);
+	}
+	
+	public function testNoColumnIdentifierGivenException() {
+		try {
+			new Tx_PtExtlist_Domain_Configuration_Columns_ColumnConfig(array('fieldIdentifier' => 'test'));
+		} catch(Exception $e) {
+			return;
+		}
+		$this->fail();
+	}
+	
+	public function testNoFieldIdentifierGivenException() {
+		try {
+			new Tx_PtExtlist_Domain_Configuration_Columns_ColumnConfig(array('columnIdentifier' => 'test'));
+		} catch(Exception $e) {
+			return;
+		}
+		$this->fail();
+	}
+	
+	
+}
+?>
