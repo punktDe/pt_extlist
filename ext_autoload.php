d2545dafd8b3917620fbe1a0dd0293870339d2c1
Michael Knoll <knoll@punkt.de>
2010-04-08 11:22:30 +0200

    Raised version to 0.0.1 dev

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * All ExtBase-conform files are included automatically. 
+ * So only files and classes from 'old' Extensions are 
+ * listed here.
+ */
+
 return array(
     'tx_pttools_objectcollection'   => t3lib_extMgm::extPath('pt_tools').'res/abstract/class.tx_pttools_objectCollection.php',
     'tx_pttools_collection'         => t3lib_extMgm::extPath('pt_tools').'res/abstract/class.tx_pttools_collection.php'
