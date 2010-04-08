2ea17725673e82e8c1d4bd8ec0bfb0c2c9e85633
Michael Knoll <knoll@punkt.de>
2010-04-08 18:25:43 +0200

    ADD: Added draft of filter config and according testcase

--- Classes/Domain/Configuration/Filters/FilterConfig.php
+++ Classes/Domain/Configuration/Filters/FilterConfig.php
@@ -1,3 +1,154 @@
 <?php
 
+class Tx_PtExtlist_Domain_Configuration_Filters_FilterConfig {
+	
+	
+	protected $filterIdentifier;
+	protected $label;
+	protected $fieldDescriptionIdentifier;
+	protected $access;
+	protected $hideColumns = array();
+	protected $resetListSortingStateOnSubmit = false;
+	protected $resetFilters = array();
+	protected $dependsOn;
+	protected $onValidated = array();
+	protected $invert;
+	protected $isActice = 1;
+	protected $value;
+	protected $settings;
+	
+	
+	
+	/**
+	 * Constructor for filter config
+	 *
+	 * @param array $filterSettings    Settings for filter
+	 */
+	public function __construct(array $filterSettings) {
+		// TODO check which values need to be set here and add assertions!
+		tx_pttools_assert::isNotEmptyString($filterSettings['filterIdentifier']);
+		
+		$this->filterIdentifier = $filterSettings['filterIdentifier'];
+	}
+	
+	
+	
+    /**
+     * @return unknown
+     */
+    public function getAccess() {
+        return $this->access;
+    }
+    
+    
+    
+    /**
+     * @return unknown
+     */
+    public function getDependsOn() {
+        return $this->dependsOn;
+    }
+    
+    
+    
+    /**
+     * @return unknown
+     */
+    public function getFieldDescriptionIdentifier() {
+        return $this->fieldDescriptionIdentifier;
+    }
+    
+    
+    
+    /**
+     * @return unknown
+     */
+    public function getFilterIdentifier() {
+        return $this->filterIdentifier;
+    }
+    
+    
+    
+    /**
+     * @return unknown
+     */
+    public function getHideColumns() {
+        return $this->hideColumns;
+    }
+    
+    
+    
+    /**
+     * @return unknown
+     */
+    public function getInvert() {
+        return $this->invert;
+    }
+    
+    
+    
+    /**
+     * @return unknown
+     */
+    public function getIsActice() {
+        return $this->isActice;
+    }
+    
+    
+    
+    /**
+     * @return unknown
+     */
+    public function getLabel() {
+        return $this->label;
+    }
+    
+    
+    
+    /**
+     * @return unknown
+     */
+    public function getOnValidated() {
+        return $this->onValidated;
+    }
+    
+    
+    
+    /**
+     * @return unknown
+     */
+    public function getResetFilters() {
+        return $this->resetFilters;
+    }
+    
+    
+    
+    /**
+     * @return unknown
+     */
+    public function getResetListSortingStateOnSubmit() {
+        return $this->resetListSortingStateOnSubmit;
+    }
+    
+    
+    
+    /**
+     * @return unknown
+     */
+    public function getSettings() {
+        return $this->settings;
+    }
+    
+    
+    
+    /**
+     * @return unknown
+     */
+    public function getValue() {
+        return $this->value;
+    }
+	
+}
+
+
 ?>
--- Classes/Domain/Configuration/Filters/FilterboxConfig.php
+++ Classes/Domain/Configuration/Filters/FilterboxConfig.php
@@ -1,3 +1,7 @@
 <?php
 
+class Tx_PtExtlist_Domain_Configuration_Filters_FilterboxConfig extends tx_pttools_objectCollection {
+	
+}
+
 ?>
