76d6b1d670243a70cf33685e7b717640d0f7241b
Michael Knoll <knoll@punkt.de>
2010-04-08 15:47:02 +0200

    ADD: Added configuration for field

--- Classes/Domain/Configuration/Data/Fields/FieldConfig.php
+++ Classes/Domain/Configuration/Data/Fields/FieldConfig.php
@@ -1,3 +1,62 @@
 <?php
 
+class Tx_PtExtlist_Domain_Configuration_Data_Fields_FieldConfig {
+	
+	protected $table;
+	
+	
+	
+	protected $field;
+	
+	
+	
+	protected $isSortable = 1;
+	
+	
+	
+	protected $access = array();
+	
+	
+	
+	public function __construct($fieldSettings) {
+		tx_pttools_assert::isNotEmptyString($fieldSettings['table']);
+		tx_pttools_assert::isNotEmptyString($fieldSettings['field']);
+		
+		$this->table = $fieldSettings['table'];
+		$this->field = $fieldSettings['field'];
+		if (array_key_exists('isSortable', $fieldSettings)) {
+			$this->isSortable = $fieldSettings['isSortable'];
+		}
+		
+		if (array_key_exists('access', $fieldSettings)) {
+			$this->access = explode(',', $fieldSettings['access']);
+		}
+	}
+	
+	
+	public function getTable() {
+		return $this->table;
+	}
+	
+	
+	
+	public function getField() {
+		return $this->field;
+	}
+	
+	
+	
+	public function getIsSortable() {
+		return $this->isSortable;
+	}
+	
+	
+	
+	public function getAccess() {
+		return $this->access;
+	}
+	
+}
+
+
 ?>
