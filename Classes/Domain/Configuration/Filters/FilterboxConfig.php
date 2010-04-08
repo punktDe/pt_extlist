d54b359b8e4f1b09072d9551e794d8bf081ecb26
Michael Knoll <knoll@punkt.de>
2010-04-08 18:29:14 +0200

    ADD: Added Filterbox config and Filterbox config collection

--- Classes/Domain/Configuration/Filters/FilterboxConfig.php
+++ Classes/Domain/Configuration/Filters/FilterboxConfig.php
@@ -1,6 +1,8 @@
 <?php
 
 class Tx_PtExtlist_Domain_Configuration_Filters_FilterboxConfig extends tx_pttools_objectCollection {
+
+	protected $restrictedClassName = 'Tx_PtExtlist_Domain_Configuration_Filters_FilterConfig';
 	
 }
 
--- Classes/Domain/Configuration/Filters/FilterboxConfigCollection.php
+++ Classes/Domain/Configuration/Filters/FilterboxConfigCollection.php
@@ -1,3 +1,9 @@
 <?php
 
+class Tx_PtExtlist_Domain_Configuration_Filters_FilterboxConfigCollection extends tx_pttools_objectCollection {
+	
+	protected $restrictedClassName = 'Tx_PtExtlist_Domain_Configuration_Filters_FilterboxConfig';
+	
+}
+
 ?>
