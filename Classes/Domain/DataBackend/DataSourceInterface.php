edbdd3274df74035e54e8be8dfe27de280232b2b
Michael Knoll <knoll@punkt.de>
2010-04-13 15:36:36 +0200

    TASK: Removed unused files, renamed directories

--- Classes/Domain/DataBackend/DataSourceInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface Tx_PtExtlist_Domain_DataBackend_DataSourceInterface {
-	
-	public function execute(Tx_PtExtlist_Domain_Query_QueryInterface $query);
-	
-}
-
-?>
--- Classes/Domain/DataBackend/MySqlBackend/Mapper.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Tx_PtExtlist_Domain_DataBackend_MySqlBackend_Mapper extends Tx_PtExtlist_Domain_DataBackend_ArrayMapper {
-
-}
-
-?>
