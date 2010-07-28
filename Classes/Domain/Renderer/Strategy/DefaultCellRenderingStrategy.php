1102e275242c42ff9c7b850eba56cd49815704e2
Christoph Ehscheidt <ehscheidt@punkt.de>
2010-07-28 15:29:58 +0200

    FIX: use corrent cell to render the content.

--- Classes/Domain/Renderer/Strategy/DefaultCellRenderingStrategy.php
+++ Classes/Domain/Renderer/Strategy/DefaultCellRenderingStrategy.php
@@ -39,7 +39,7 @@ class Tx_PtExtlist_Domain_Renderer_Strategy_DefaultCellRenderingStrategy impleme
 	public function renderCell($fieldIdentifier, $columnIndex, Tx_PtExtlist_Domain_Model_List_Row $currentRow) {
 			
 		$columnConfig = $this->rendererConfiguration->getColumnConfigCollection()->getColumnConfigByIdentifier($columnIndex);
-		$content = $currentRow->getItemByIndex($fieldIdentifier);
+		$content = $currentRow->getItemById($fieldIdentifier);
 		
 		$fieldSet = $this->createFieldSet($currentRow);
 		
