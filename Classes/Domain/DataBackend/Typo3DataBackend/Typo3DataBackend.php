5bea7684e60338413623d6fafeacfcdee2e380e1
Michael Knoll <knoll@punkt.de>
2010-07-29 11:35:52 +0200

    FIX: Fixed class for t3 data backend

--- Classes/Domain/DataBackend/Typo3DataBackend/Typo3DataBackend.php
+++ Classes/Domain/DataBackend/Typo3DataBackend/Typo3DataBackend.php
@@ -31,7 +31,7 @@
  * @subpackage pt_extlist
  *
  */
-class Tx_PtExtlist_Domain_DataBackend_Typo3DataBackend_Typo3DataBackend extends Tx_PtExtlist_Domain_DataBackend_DataSource_MySqlDataSource {
+class Tx_PtExtlist_Domain_DataBackend_Typo3DataBackend_Typo3DataBackend extends Tx_PtExtlist_Domain_DataBackend_MySqlDataBackend_MySqlDataBackend {
 
 	// Nothing to implement here so far, as data source has the same interface for mysql and t3 db!
 	
