3a918cda772fd8793861478d54b3d881aa32ea61
Christoph Ehscheidt <ehscheidt@punkt.de>
2010-07-29 16:25:31 +0200

    ADD: tests for abstract data backend

---
+++ Tests/Domain/DataBackend/AbstractDataBackend_testcase.php
@@ -0,0 +1,57 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2010 Daniel Lienert <lienert@punkt.de>, Michael Knoll <knoll@punkt.de>,
+*  Christoph Ehscheidt <ehscheidt@punkt.de>
+*  All rights reserved
+*
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+class Tx_PtExtlist_Tests_Domain_DataBackend_AbstractDataBackend_testcase extends Tx_PtExtlist_Tests_BaseTestcase {
+
+	public function setUp() {
+		$this->initDefaultConfigurationBuilderMock();
+	}
+	
+	
+	public function testRegisterObserver() {
+		$dataBackend = $this->getMockForAbstractClass('Tx_PtExtlist_Domain_DataBackend_AbstractDataBackend',array($this->configurationBuilderMock));
+		
+		$this->assertTrue(method_exists($dataBackend,'registerObserver'));
+	}
+	
+	
+	
+	public function testNotify() {
+		$dataBackendClass = $this->buildAccessibleProxy('Tx_PtExtlist_Domain_DataBackend_AbstractDataBackend');
+		$dataBackend = $this->getMockForAbstractClass($dataBackendClass,array($this->configurationBuilderMock));
+		
+		$pager = $this->getMock('Tx_PtExtlist_Domain_Model_Pager_DefaultPager',array('updateItemCount'));
+		$pager->expects($this->once())
+				->method('updateItemCount')
+				->with(111);
+		
+		$dataBackend->_call(registerObserver,$pager);
+		
+		$dataBackend->_call(updateObserversItemCount, 111);
+	}
+}
+
+?>
