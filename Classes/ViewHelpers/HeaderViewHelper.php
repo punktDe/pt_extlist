97aeff3c4c6d71b88ecf1a4aabcfc2ea5c5bea3b
Christoph Ehscheidt <ehscheidt@punkt.de>
2010-08-02 14:29:18 +0200

    CHG: HeaderViewHelper with images

--- Classes/ViewHelpers/HeaderViewHelper.php
+++ Classes/ViewHelpers/HeaderViewHelper.php
@@ -26,13 +26,16 @@
 
 class Tx_PtExtlist_ViewHelpers_HeaderViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
 
+	
+	
 	/**
 	 * 
 	 * Enter description here ...
 	 * @param $header
 	 * @param $captions
+	 * @param $key string
 	 */
-	public function render(Tx_PtExtlist_Domain_Model_List_Header_ListHeader $headers, Tx_PtExtlist_Domain_Model_List_Row $captions) {
+	public function render(Tx_PtExtlist_Domain_Model_List_Header_ListHeader $headers, Tx_PtExtlist_Domain_Model_List_Row $captions, $key='header') {
 		$output = '';
 		if ($headers === NULL || $captions === NULL) {
 			return '';
@@ -40,14 +43,19 @@ class Tx_PtExtlist_ViewHelpers_HeaderViewHelper extends Tx_Fluid_Core_ViewHelper
 		
 		$output = '';
 		foreach ($headers as $header) {
-			$this->templateVariableContainer->add('label', $captions->getItemById($header->getColumnIdentifier()));
-			
-			$this->templateVariableContainer->add('sorting', $this->renderSorting($header));
+			$this->templateVariableContainer->add('label', $captions->getItemById($header->getColumnIdentifier()));		
+			$this->templateVariableContainer->add('sortingImg', $this->renderSorting($header));
+			$this->templateVariableContainer->add('state', $header->getSortingState());
+			$this->templateVariableContainer->add('column', $header->getColumnIdentifier());
+			$this->templateVariableContainer->add('sortable', $header->isSortable());
 			
 			$output .= $this->renderChildren();
 			
 			$this->templateVariableContainer->remove('label');
-			$this->templateVariableContainer->remove('sorting');
+			$this->templateVariableContainer->remove('state');
+			$this->templateVariableContainer->remove('column');
+			$this->templateVariableContainer->remove('sortable');
+			$this->templateVariableContainer->remove('sortingImg');
 			
 		}
 		return $output;
@@ -55,11 +63,15 @@ class Tx_PtExtlist_ViewHelpers_HeaderViewHelper extends Tx_Fluid_Core_ViewHelper
 	}
 	
 	protected function renderSorting(Tx_PtExtlist_Domain_Model_List_Header_HeaderColumn $header) {
-		if($header->getSortingState() == Tx_PtExtlist_Domain_QueryObject_Query::SORTINGSTATE_ASC) {
-			return 'ASC';
+
+		if(!$header->isSortable()) {
+			return 'typo3conf/ext/pt_extlist/Resources/Public/List/icon_table_sort_default.png';
+		} elseif($header->getSortingState() == Tx_PtExtlist_Domain_QueryObject_Query::SORTINGSTATE_ASC) {
+			return 'typo3conf/ext/pt_extlist/Resources/Resources/Public/List/icon_table_sort_asc.png';
 		} else {
-			return 'DESC';
+			return 'typo3conf/ext/pt_extlist/Resources/Resources/Public/List/icon_table_sort_desc.png';
 		}
+
 	}
 	
 	
