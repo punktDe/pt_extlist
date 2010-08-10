6cce4ba2e0b10d4c2d0876a331b1780652755e9d
Michael Knoll <mimi@kaktusteam.de>
2010-08-10 03:53:08 +0200

    RFT: Refactored string filter and abstract single value filter

--- Classes/Domain/Model/Filter/AbstractSingleValueFilter.php
+++ Classes/Domain/Model/Filter/AbstractSingleValueFilter.php
@@ -32,4 +32,130 @@
  */
 abstract class Tx_PtExtlist_Domain_Model_Filter_AbstractSingleValueFilter extends Tx_PtExtlist_Domain_Model_Filter_AbstractFilter {
 
+	/**
+     * Holds the current filter value
+     *
+     * @var string
+     */
+    protected $filterValue = '';
+    
+    
+    
+    /**
+     * Identifier of field on which this filter is operating (database field to be filtered)
+     *
+     * @var string
+     */
+    protected $fieldDescriptionIdentifier;
+    
+    
+    
+    /**
+     * Returns raw value of filter (NOT FILTER QUERY!!!)
+     *
+     * @return string
+     */
+    public function getFilterValue() {
+        return $this->filterValue;
+    }   
+    
+    
+    
+    /**
+     * Returns field description identifier on which this filter operates
+     *
+     * @return string Field description Identifier
+     */
+    public function getFieldDescriptionIdentifier() {
+        return $this->fieldDescriptionIdentifier;
+    }
+    
+    
+    
+    /**
+     * Persists filter state to session
+     *
+     * @return array Array of filter data to persist to session
+     */
+    public function persistToSession() {
+        return array('filterValue' => $this->filterValue);
+    }
+    
+    
+    
+    /**
+     * Resets filter to its default values
+     * 
+     * @return void
+     */
+    public function reset() {
+        $this->filterValue = '';
+        $this->resetSessionDataForFilter();
+        $this->resetGpVarDataForFilter();
+        $this->init();
+    }
+    
+    
+    
+    /**
+     * Resets session data for this filter
+     */
+    protected function resetSessionDataForFilter() {
+        $this->sessionFilterData = array();
+    }
+    
+    
+    
+    /**
+     * Resets get/post var data for this filter
+     */
+    protected function resetGpVarDataForFilter() {
+        $this->gpVarFilterData = array();
+    }
+    
+    
+    
+    /**
+     * Returns an error message for this filter
+     *
+     * @return string
+     */
+    public function getErrorMessage() {
+        return $this->errorMessage;
+    }
+    
+    
+
+    /**
+     * Template method for initializing filter by get / post vars
+     */
+    protected function initFilterByGpVars() {
+        if(array_key_exists('filterValue', $this->gpVarFilterData)) {
+            $this->filterValue = $this->gpVarFilterData['filterValue']; 
+        }
+    }
+    
+    
+
+    /**
+     * Template method for initializing filter by session data
+     */
+    protected function initFilterBySession() {
+        $this->filterValue = array_key_exists('filterValue', $this->sessionFilterData) ? $this->sessionFilterData['filterValue'] : $this->filterValue;
+    }
+    
+    
+    
+    /**
+     * Template method for initializing filter by TS configuration
+     */
+    protected function initFilterByTsConfig() {
+        $this->filterValue = $this->filterConfig->getDefaultValue() ? $this->filterConfig->getDefaultValue() : $this->filterValue;
+        
+        if ($this->filterConfig->getFieldDescriptionIdentifier() == '') {
+            throw new Exception('No fieldDescriptionIdentifier set in TS config for filter ' . $this->getFilterIdentifier() . ' 1280762513');
+        }
+        $this->fieldDescriptionIdentifier = $this->filterConfig->getFieldDescriptionIdentifier();
+    }
+	
 }
