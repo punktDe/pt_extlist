9cf63f5cec4e50e3e77acc7df4dcb0f643449968
Christoph Ehscheidt <ehscheidt@punkt.de>
2010-08-17 13:20:34 +0200

    FIX: demolist renderer userfunc

--- Resources/Private/UserFunctions/class.tx_ptextlist_demolist_renderer.php
+++ Resources/Private/UserFunctions/class.tx_ptextlist_demolist_renderer.php
@@ -47,7 +47,7 @@ class tx_ptextlist_demolist_renderer {
 	
 		$currentContent = $params['currentContent'];
 		
-		if (isset($values['iso2'].getValue())) {
+		if (isset($values['iso2'])) {
 			// check if file exists
 			$flagFileName =  'gfx/flags/' . strtolower($values['iso2']) . '.gif';
 			if (is_file(PATH_typo3 . $flagFileName)) {
