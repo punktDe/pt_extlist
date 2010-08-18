194e954ef9219ddf957bf03443a4cb43406804da
Christoph Ehscheidt <ehscheidt@punkt.de>
2010-08-18 10:24:13 +0200

    ADD: pager collection and test

---
+++ Tests/Domain/Model/Pager/PagerCollectionTest.php
@@ -0,0 +1,42 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2010 Daniel Lienert <lienert@punkt.de>, Michael Knoll <knoll@punkt.de>,
+*  Christoph Ehscheidt <ehscheidt@punkt.de>
+*  All rights reserved
+*
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+class Tx_PtExtlist_Tests_Domain_Model_Pager_PagerCollectionTest extends Tx_Extbase_BaseTestCase {
+
+	public function setUp() {}
+	
+	public function testSetCurrentPage() {
+		$pager = $this->getMock('Tx_PtExtlist_Domain_Model_Pager_DefaultPager', array('setCurrentPage'), array(),'',false, false, false);
+		$pager->expects($this->once())->method('setCurrentPage')->with(42);
+		
+		$collection = new Tx_PtExtlist_Domain_Model_Pager_PagerCollection();
+		$collection->addItem($pager);
+		
+		$collection->setCurrentPage(42);
+	}
+}
+
+?>
