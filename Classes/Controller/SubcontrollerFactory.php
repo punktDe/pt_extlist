d6d72755e06ad7617befedd52c44a9a0570f91fc
Michael Knoll <knoll@punkt.de>
2010-08-31 13:45:46 +0200

    ADD: Added factory for subcontrollers and testcase

---
+++ Classes/Controller/SubcontrollerFactory.php
@@ -0,0 +1,82 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2010 Daniel Lienert <lienert@punkt.de>, Michael Knoll <knoll@punkt.de>
+*  All rights reserved
+*
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Factory for creating instances of pt_extlist controllers to be used as subcontrollers
+ * 
+ * @author Michael Knoll <knoll@punkt.de>
+ * @author Daniel Lienert <lienert@punkt.de>
+ * @package TYPO3
+ * @subpackage pt_extlist
+ */
+class Tx_PtExtlist_Controller_SubcontrollerFactory {
+
+	/**
+	 * Holds associative array of instances listIdentifier => factoryInstance
+	 *
+	 * @var array
+	 */
+	protected static $instancesArray = array();
+	
+	
+	
+	/**
+	 * Holds identifier of list, this factory creates controllers for
+	 *
+	 * @var string
+	 */
+	protected $listIdentifier;
+	
+
+	
+	
+	/**
+	 * Factory method for subcontrollerFactory
+	 *
+	 * @param string $listIdentifier
+	 * @return Tx_PtExtlist_Controller_SubcontrollerFactory
+	 */
+	public static function getInstanceByListIdentifier($listIdentifier) {
+	    tx_pttools_assert::isNotEmptyString($listIdentifier, array('message' => 'Cannot instantiate subcontroller factory with empty list identifier! 1283254996'));
+	    
+		if(self::$instancesArray[$listIdentifier] === NULL) {
+	   	   	self::$instancesArray[$listIdentifier] = new Tx_PtExtlist_Controller_SubcontrollerFactory($listIdentifier);
+	    }
+	    
+	    return self::$instancesArray[$listIdentifier];
+	}
+	
+	
+	
+	/**
+	 * Constructor for subcontroller factory
+	 *
+	 * @param string $listIdentifier Identifier of list, factory should create subcontrollers for
+	 */
+	protected function __construct($listIdentifier) {
+		$this->listIdentifier = $listIdentifier;
+	}
+	
+}
