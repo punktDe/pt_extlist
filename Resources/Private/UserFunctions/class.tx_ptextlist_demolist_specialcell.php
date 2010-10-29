d9f548e0e0377a46c4e0f3be01a49f7c24858f57
Simon Schaufelberger <schaufelberger@punkt.de>
2010-10-29 16:09:07 +0200

    TASK: changed end of line to unix

--- Resources/Private/UserFunctions/class.tx_ptextlist_demolist_specialcell.php
+++ Resources/Private/UserFunctions/class.tx_ptextlist_demolist_specialcell.php
@@ -28,10 +28,10 @@ class tx_ptextlist_demolist_specialcell {
 
 	/**
 	 * Returns an array with special values for a cell.
-	 * 
+	 *
 	 * array('key1' => 'value1', 'key2' => 'value2')
-	 * 
-	 * 
+	 *
+	 *
 	 * @param Tx_PtExtlist_Domain_Model_List_Cell &$cell
 	 * @return array
 	 */
@@ -39,44 +39,44 @@ class tx_ptextlist_demolist_specialcell {
 		$columnIndex = $cell->getColumnIndex();
 		$rowIndex = $cell->getRowIndex();
 		$content = $cell->getValue();
-		
+
 		$specialValues = array();
 
 		$color = self::fadeColor($columnIndex, $rowIndex);
-		
+
 		$specialValues['style'] = "background-color:rgb($color[0],$color[1],$color[2]);";
 
 		return $specialValues;
 	}
-	
+
 	public static function processGrey(Tx_PtExtlist_Domain_Model_List_Cell &$cell) {
-	
+
 		$color[] = 161;
 		$color[] = 255;
 		$color[] = 161;
-		
+
 		$specialValues['style'] = "background-color:rgb($color[0],$color[1],$color[2]);";
 
 		return $specialValues;
 	}
-	
+
 	public static function fadeColor($index, $row, $items = 5, $rowItems = 10) {
-		
+
 		$offset=floor((161/$rowItems)*$row);
-		
+
 		$s  = array(161-$offset,161-$offset,161-$offset);
 		$e  = array(255-$offset,255-$offset,255-$offset);
-		
+
 		$r = array();
 		$r[0] = ceil(max(0,$s[0]-((($e[0]-$s[0])/-$items)*$index)));
 		$r[0] = 255-$offset;
 	    $r[1] = ceil(max(0,$s[1]-((($e[1]-$s[1])/-$items)*$index)));
 	    $r[2] = ceil(max(0,$s[2]-((($e[2]-$s[2])/-$items)*$index)));
 //	    $r[2] = 200-$offset;
-		    
-		return $r;  
+
+		return $r;
 	}
-	
+
 }
 
 ?>
