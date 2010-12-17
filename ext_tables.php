dcaa3b944b48ee31e3f6b079b04af15501c6ea06
Daniel Lienert <daniel@lienert.cc>
2010-12-17 16:28:22 +0100

    ADD classes to store the pluginstate as extbase object

--- ext_tables.php
+++ ext_tables.php
@@ -64,5 +64,28 @@ $TCA['tx_ptextlist_domain_model_bookmarks_bookmark'] = array (
     )
 );
 
+t3lib_extMgm::addLLrefForTCAdescr('Tx_PtExtlist_Domain_Model_StateStorage_State', 'EXT:pt_extlist/Resources/Private/Language/locallang_csh_tx_ptextlist_domain_model_state.xml');
+t3lib_extMgm::allowTableOnStandardPages('Tx_PtExtlist_Domain_Model_StateStorage_State');
+$TCA['tx_ptextlist_domain_model_state'] = array (
+	'ctrl' => array (
+		'title'             => 'LLL:EXT:pt_extlist/Resources/Private/Language/locallang_db.xml:tx_ptextlist_domain_model_statestorage_state',
+		'label' 			=> 'hash',
+		'tstamp' 			=> 'tstamp',
+		'crdate' 			=> 'crdate',
+		'versioningWS' 		=> 2,
+		'versioning_followPages'	=> TRUE,
+		'origUid' 			=> 't3_origuid',
+		'languageField' 	=> 'sys_language_uid',
+		'transOrigPointerField' 	=> 'l18n_parent',
+		'transOrigDiffSourceField' 	=> 'l18n_diffsource',
+		'delete' 			=> 'deleted',
+		'enablecolumns' 	=> array(
+			'disabled' => 'hidden'
+			),
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/State.php',
+		'iconfile' 			=> t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_ptextlist_domain_model_statestorage_state'
+	)
+);
+
 
 ?>
