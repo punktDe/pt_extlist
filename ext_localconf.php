0215d1ec52ff8d9e25cc7798d7124a91391bcbfa
Daniel Lienert <daniel@lienert.cc>
2011-01-27 17:38:51 +0100

    FIX: Localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -24,8 +24,7 @@ Tx_Extbase_Utility_Extension::configurePlugin(
 	$_EXTKEY,																		// The extension name (in UpperCamelCase) or the extension key (in lower_underscore)
 	'Pi1',																			// A unique name of the plugin in UpperCamelCase
 	$controllerActions,
-	$controllerActions,
-	Tx_Extbase_Utility_Extension::PLUGIN_TYPE_CONTENT_ELEMENT
+	$controllerActions
 );
 
 Tx_Extbase_Utility_Extension::configurePlugin(
