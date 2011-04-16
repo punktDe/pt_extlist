2fffcc38ccc66d748cd1a23b697a79ddf2cb01bf
Daniel Lienert <daniel@lienert.cc>
2011-04-16 10:36:41 +0200

    ADD: Test for baseConfig

---
+++ Tests/Domain/Configuration/Base/BaseConfigTest.php
@@ -0,0 +1,74 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2010-2011 punkt.de GmbH - Karlsruhe, Germany - http://www.punkt.de
+ *  Authors: Daniel Lienert, Michael Knoll, Christoph Ehscheidt
+ *  All rights reserved
+ *
+ *  For further information: http://extlist.punkt.de <extlist@punkt.de>
+ *
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Testcase for base configuration
+ *
+ * @package Tests
+ * @subpackage Domain\Configuration\Base
+ * @author Daniel Lienert
+ */
+class Tx_PtExtlist_Tests_Domain_Configuration_Base_BaseConfig_testcase extends Tx_PtExtlist_Tests_BaseTestcase {
+
+	
+	/**
+	 * @var Tx_PtExtlist_Domain_Configuration_Base_BaseConfig
+	 */
+	protected $baseConfig;
+	
+	
+	public function setup() {
+		$this->initDefaultConfigurationBuilderMock();
+		
+		$configArray = array('uncachedSessionStorageAdapter' => 'uncachedSessionStorageAdapter',
+							'cachedSessionStorageAdapter' => 'cachedSessionStorageAdapter'
+							);
+		
+		$this->baseConfig = new Tx_PtExtlist_Domain_Configuration_Base_BaseConfig($this->configurationBuilderMock, $configArray);
+	}
+	
+	
+	/**
+	 * @test
+	 */
+	public function getCachedSessionStorageAdapter() {
+		$this->assertEquals($this->baseConfig->getCachedSessionStorageAdapter(), 'cachedSessionStorageAdapter');
+	}
+	
+	
+	
+	/**
+	 * @test
+	 */
+	public function getUncachedSessionStorageAdapter() {
+		$this->assertEquals($this->baseConfig->getUncachedSessionStorageAdapter(), 'uncachedSessionStorageAdapter');
+	}
+	
+}
+
+?>
