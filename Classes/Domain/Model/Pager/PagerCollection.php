d9df4927d4f9a8519faa66a4240411e61e55574e
Michael Knoll <mimi@kaktusteam.de>
2011-04-29 01:41:09 +0200

    FIX: if page == 1 this is stored to session. Perhaps it has strange effects, as it's not been done yet.

--- Classes/Domain/Model/Pager/PagerCollection.php
+++ Classes/Domain/Model/Pager/PagerCollection.php
@@ -204,9 +204,10 @@ class Tx_PtExtlist_Domain_Model_Pager_PagerCollection extends tx_pttools_collect
 	 * @see Classes/Domain/StateAdapter/Tx_PtExtlist_Domain_StateAdapter_SessionPersistableInterface::persistToSession()
 	 */
 	public function persistToSession() {
-		if($this->currentPage != 1) {
+		// TODO why is first page not stored to session?
+		#if($this->currentPage != 1) {
 			return array('page' => $this->currentPage);	
-		}
+		#}
 	}
 
 
