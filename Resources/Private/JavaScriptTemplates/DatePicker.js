a27d41a7a627454db3aba64cb65f6e7f12e255c3
Joachim Mathes <mathes@punkt.de>
2011-10-19 17:29:05 +0200

    ADD: DatePicker JS functionality

--- Resources/Private/JavaScriptTemplates/DatePicker.js
+++ Resources/Private/JavaScriptTemplates/DatePicker.js
@@ -1,22 +1,38 @@
 jQuery(function($) {
+
+	var options = ###options###;
+	var filterValue = "###filterValue###";
+	var defaultDate = new Date(filterValue.split('-')[0], filterValue.split('-')[1] - 1, filterValue.split('-')[2]);
 	var datePickerOptions = {
+		altFormat: "yymmdd",
+		altField: "#datePickerFilter",
+		defaultDate: defaultDate,
 		onSelect: submitFilterValue,
+		beforeShowDay: evaluateTimeSpans,
 		changeMonth: true,
 		changeYear: true
 	};
 
 	function submitFilterValue(date, datePicker) {
-		var dateElements = date.split('.');
-		var selectedDate = new Date(dateElements[2], dateElements[1] - 1, dateElements[0]);
-		var startTimestamp = selectedDate.getTime() / 1000;
-		var endTimestamp = startTimestamp + 86399;
-
-		$("#datePickerFilter").val(startTimestamp + ',' + endTimestamp);
-
-		form = "tx-ptextlist-filterbox-form-###filterBoxIdentifier###";
+		var form = "tx-ptextlist-filterbox-form-###filterBoxIdentifier###";
 		document.forms[form].submit();
 	}
 
-	$("#datepicker").datepicker(datePickerOptions);
+	function evaluateTimeSpans(date) {
+		var year = date.getFullYear();
+		var month = date.getMonth() + 1;
+		var day = date.getDate();
+		var dateInAltFormat = year.toString() + month.toString() + day.toString();
+
+		for (var i = 0; i < options.timeSpans.length; i++) {
+			if (options.timeSpans[i].start <= dateInAltFormat && options.timeSpans[i].end >= dateInAltFormat) {
+				return [true, "ptextlist-event-date"];
+			}
+		}
+		
+		return [false, ''];
+	}
+
+	$("#datepicker").datepicker(jQuery.extend(datePickerOptions, $.datepicker.regional['###regional###']));
 
 });
