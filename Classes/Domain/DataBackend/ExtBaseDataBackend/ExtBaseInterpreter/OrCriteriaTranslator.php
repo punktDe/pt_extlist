f9bc42bce40b472862469ec2ba064548dfff431c
Michael Knoll <knoll@punkt.de>
2012-01-12 12:57:06 +0100

    ADD: Added OR criteria translation for extbase. EXPERIMENTAL, not yet tested

--- Classes/Domain/DataBackend/ExtBaseDataBackend/ExtBaseInterpreter/OrCriteriaTranslator.php
+++ Classes/Domain/DataBackend/ExtBaseDataBackend/ExtBaseInterpreter/OrCriteriaTranslator.php
@@ -36,21 +36,43 @@
  class Tx_PtExtlist_Domain_DataBackend_ExtBaseDataBackend_ExtBaseInterpreter_OrCriteriaTranslator 
     implements Tx_PtExtlist_Domain_DataBackend_ExtBaseDataBackend_ExtBaseInterpreter_ExtBaseCriteriaTranslatorInterface {
 	
-	/**
-     * Translates a query an manipulates given query object
-     *
-     * @param Tx_PtExtlist_Domain_QueryObject_Criteria $criteria Criteria to be translated
-     * @param Tx_Extbase_Persistence_Query $extbaseQuery Query to add criteria to
-     * @param Tx_Extbase_Persistence_Repository $extbaseRepository Associated repository
-     */
-    public static function translateCriteria(
-           Tx_PtExtlist_Domain_QueryObject_Criteria $criteria,
-           Tx_Extbase_Persistence_Query $extbaseQuery,
-           Tx_Extbase_Persistence_Repository $extbaseRepository) {
+	 /**
+	      * Translates a query an manipulates given query object
+	      *
+	      * @param Tx_PtExtlist_Domain_QueryObject_Criteria $criteria Criteria to be translated
+	      * @param Tx_Extbase_Persistence_Query $extbaseQuery Query to add criteria to
+	      * @param Tx_Extbase_Persistence_Repository $extbaseRepository Associated repository
+	      */
+	     public static function translateCriteria(
+	            Tx_PtExtlist_Domain_QueryObject_Criteria $criteria,
+	            Tx_Extbase_Persistence_Query $extbaseQuery,
+	            Tx_Extbase_Persistence_Repository $extbaseRepository) {
 
-        throw new Exception("OR-Criteria cannot yet be translated by Extbase Interpreter! 1299228163");
-           	
-	}
+	         /**
+	          * This is a little ugly here:
+	          *
+	          * As we do not create Extbase criterias from our generic pt_extlist criterias
+	          * but set the criterias directly on the created extbase query, we have to cheat
+	          * here and generate two helper queries, whenever a OR query has to be translated.
+	          *
+	          * After having translated the two criterias of the generic OR criteria, we
+	          * put them together again in a single extbase query.
+	          */
+	         $tmpQuery1 = $extbaseRepository->createQuery();
+	         $tmpQuery2 = $extbaseRepository->createQuery();
+	         // translate first OR criteria by creating a new extbase query
+	         $tmpQuery1 = Tx_PtExtlist_Domain_DataBackend_ExtBaseDataBackend_ExtBaseInterpreter_ExtBaseInterpreter::setCriteriaOnExtBaseQueryByCriteria($criteria->getFirstCriteria(), $tmpQuery1, $extbaseRepository);
+	         // translate second OR criteria by creating a new extbase query
+	         $tmpQuery2 = Tx_PtExtlist_Domain_DataBackend_ExtBaseDataBackend_ExtBaseInterpreter_ExtBaseInterpreter::setCriteriaOnExtBaseQueryByCriteria($criteria->getSecondCriteria(), $tmpQuery2, $extbaseRepository);
+	         // put both translated criterias together again in a single extbase query
+	         if ($extbaseQuery->getConstraint()) {
+	             $extbaseQuery->matching($extbaseQuery->logicalAnd($extbaseQuery->getConstraint(),
+	                 $extbaseQuery->logicalOr($tmpQuery1->getConstraint(), $tmpQuery2->getConstraint())));
+	         } else {
+	         	$extbaseQuery->matching($extbaseQuery->logicalOr($tmpQuery1->getConstraint(), $tmpQuery2->getConstraint()));
+	         }
+	         return $extbaseQuery;
+	 	}
 
  }
  
