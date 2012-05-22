7da57d976aef1ddb1ee5dc59d50f882aba5b4d6d
Joachim Mathes <mathes@punkt.de>
2012-05-22 10:15:45 +0200

    ADD: clear alt field of DateRangeFilter, if user clears input field

--- Resources/Private/JavaScriptTemplates/DateRangeDatePicker.js
+++ Resources/Private/JavaScriptTemplates/DateRangeDatePicker.js
@@ -9,6 +9,13 @@ jQuery(function($) {
 		altField: "#typo3-ptextlist-filter-daterangedatepicker-daterangeto"
 	};
 
+	$("#typo3-ptextlist-filter-daterangedatepicker-from").change(function(){
+		if (!$(this).val()) $("#typo3-ptextlist-filter-daterangedatepicker-daterangefrom").val('');
+	});
+	$("#typo3-ptextlist-filter-daterangedatepicker-to").change(function(){
+		if (!$(this).val()) $("#typo3-ptextlist-filter-daterangedatepicker-daterangeto").val('');
+	});
+
 	$('#typo3-ptextlist-filter-daterangedatepicker-from').datepicker(datePickerFromOptions);
 	$('#typo3-ptextlist-filter-daterangedatepicker-to').datepicker(datePickerToOptions);
 });
