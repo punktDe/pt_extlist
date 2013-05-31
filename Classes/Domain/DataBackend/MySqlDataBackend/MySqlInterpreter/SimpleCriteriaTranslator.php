a31f76316e7bb54dfcda3858aa99b9bd6510e3bd
Daniel Lienert <daniel@lienert.cc>
2013-05-31 18:34:06 +0200

    FIX: PHP5.4 Bug

--- Classes/Domain/DataBackend/MySqlDataBackend/MySqlInterpreter/SimpleCriteriaTranslator.php
+++ Classes/Domain/DataBackend/MySqlDataBackend/MySqlInterpreter/SimpleCriteriaTranslator.php
@@ -59,7 +59,9 @@ class Tx_PtExtlist_Domain_DataBackend_MySqlDataBackend_MySqlInterpreter_SimpleCr
 		if (is_array($value)) {
 			$escapedValues = self::escapeArray($value);
 			$returnString = '("' . implode('", "', $escapedValues) . '")';
-        } else {
+        } elseif(is_numeric($value)) {
+			$returnString = $value;
+		} else {
 			$returnString = self::wrapNonNumericValue(mysql_real_escape_string($value));
 		}
 		return $returnString;
