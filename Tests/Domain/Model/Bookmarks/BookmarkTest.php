b846aea474d7747171611aee70b9f7fa4ba00fd8
Christiane Helmchen <Jashi@gmx.com>
2013-07-03 17:19:23 +0200

    ADD: basic bookmark model setter/getter tests

--- Tests/Domain/Model/Bookmarks/BookmarkTest.php
+++ Tests/Domain/Model/Bookmarks/BookmarkTest.php
@@ -30,10 +30,10 @@
  * Testcase for bookmark domain object
  *
  * @package Tests
- * @subpackage Domain\Model\Bookmarks
+ * @subpackage Domain\Model\Bookmark
  * @author Michael Knoll 
  */
-class Tx_PtExtlist_Tests_Domain_Model_Bookmarks_Bookmark_testcase extends Tx_PtExtlist_Tests_BaseTestcase {
+class Tx_PtExtlist_Tests_Domain_Model_Bookmark_Bookmark_testcase extends Tx_PtExtlist_Tests_BaseTestcase {
 
 	/**
 	 * @var string
@@ -43,14 +43,14 @@ class Tx_PtExtlist_Tests_Domain_Model_Bookmarks_Bookmark_testcase extends Tx_PtE
 
 
 	/**
-	 * @var Tx_PtExtlist_Domain_Model_Bookmarks_Bookmark
+	 * @var Tx_PtExtlist_Domain_Model_Bookmark_Bookmark
 	 */
 	protected $proxy;
 
 
 
 	public function setUp() {
-		$this->proxyClass = $this->buildAccessibleProxy('Tx_PtExtlist_Domain_Model_Bookmarks_Bookmark');
+		$this->proxyClass = $this->buildAccessibleProxy('Tx_PtExtlist_Domain_Model_Bookmark_Bookmark');
 		$this->proxy = new $this->proxyClass;
 	}
 
@@ -67,8 +67,205 @@ class Tx_PtExtlist_Tests_Domain_Model_Bookmarks_Bookmark_testcase extends Tx_PtE
 	 * @test
 	 */
 	public function classExists() {
-		$this->assertTrue(class_exists('Tx_PtExtlist_Domain_Model_Bookmarks_Bookmark'));
+		$this->assertTrue(class_exists('Tx_PtExtlist_Domain_Model_Bookmark_Bookmark'));
 	}
+
+
+
+	/**
+	 * @test
+	 */
+	public function canAccessConstants(){
+		$className = 'Tx_PtExtlist_Domain_Model_Bookmark_Bookmark';
+		$constantName = 'PTEXTLIST_BOOKMARK_PUBLIC';
+		$expected = 1;
+		$actual = constant(sprintf('%s::%s', $className, $constantName));
+		$this->assertEquals($expected, $actual);
+	}
+
+
+
+	/**
+	 * @test
+	 */
+	public function getTypeGetsType() {
+		$expected = 2;
+        $this->proxy->_set('type', $expected);
+		$actual = $this->proxy->getType();
+        $this->assertEquals($expected, $actual);
+    }
+
+
+
+	/**
+	 * @test
+	 */
+	public function setTypeSetsType() {
+		$expected = 3;
+		//$expected = Tx_PtExtlist_Domain_Model_Bookmark_Bookmark::PTEXTLIST_BOOKMARK_PUBLIC;
+		$this->proxy->setType($expected);
+		$actual = $this->proxy->_get('type');
+		$this->assertEquals($expected, $actual);
+	}
+
+
+
+	/**
+	 * @test
+	 */
+	public function getNameGetsName() {
+		$expected = 'myname';
+        $this->proxy->_set('name', $expected);
+		$actual = $this->proxy->getName();
+        $this->assertEquals($expected, $actual);
+    }
+
+
+
+	/**
+	 * @test
+	 */
+	public function setNameSetsName() {
+		$expected = 'notmyname';
+		$this->proxy->setName($expected);
+		$actual = $this->proxy->_get('name');
+		$this->assertEquals($expected, $actual);
+	}
+
+
+
+	/**
+	 * @test
+	 */
+	public function getListIdGetsListId() {
+		$expected = 'listid';
+        $this->proxy->_set('listId', $expected);
+		$actual = $this->proxy->getListId();
+        $this->assertEquals($expected, $actual);
+    }
+
+
+
+	/**
+	 * @test
+	 */
+	public function setListIdSetsListId() {
+		$expected = 'listid';
+		$this->proxy->setListId($expected);
+		$actual = $this->proxy->_get('listId');
+		$this->assertEquals($expected, $actual);
+	}
+
+
+
+//	/**
+//	 * @test
+//	 */
+//	public function getFeGroupGetsFeGroup() {
+//		$expected = 'feGroup';
+//        $this->proxy->_set('listId', $expected);
+//		$actual = $this->proxy->getListId();
+//        $this->assertEquals($expected, $actual);
+//    }
+
+
+//    public function testGetSetFeGroup() {
+//        $bookmark = new Tx_PtExtlist_Domain_Model_Bookmark_Bookmark();
+//        $feGroupMock = $this->getMock('Tx_Extbase_Domain_Model_FrontendUserGroup');
+//        $bookmark->setFeGroup($feGroupMock);
+//        $this->assertEquals($bookmark->getFeGroup(), $feGroupMock);
+//    }
+
+
+//	/**
+//	 * @test
+//	 */
+//	public function setListIdSetsListId() {
+//		$expected = 'listid';
+//		$this->proxy->setListId($expected);
+//		$actual = $this->proxy->_get('listId');
+//		$this->assertEquals($expected, $actual);
+//	}
+
+
+
+//	/**
+//	 * @test
+//	 */
+//	public function getListIdGetsListId() {
+//		$expected = 'listid';
+//        $this->proxy->_set('listId', $expected);
+//		$actual = $this->proxy->getListId();
+//        $this->assertEquals($expected, $actual);
+//    }
+
+//
+//
+//    public function testGetSetFeUser() {
+//        $feUserMock = $this->getMock('Tx_Extbase_Domain_Model_FrontendUser');
+//        $bookmark = new Tx_PtExtlist_Domain_Model_Bookmark_Bookmark();
+//        $bookmark->setFeUser($feUserMock);
+//        $this->assertEquals($bookmark->getFeUser(), $feUserMock);
+//    }
+
+//	/**
+//	 * @test
+//	 */
+//	public function setListIdSetsListId() {
+//		$expected = 'listid';
+//		$this->proxy->setListId($expected);
+//		$actual = $this->proxy->_get('listId');
+//		$this->assertEquals($expected, $actual);
+//	}
+
+
+
+	/**
+	 * @test
+	 */
+	public function getDescriptionGetsDescription() {
+		$expected = 'desc';
+        $this->proxy->_set('description', $expected);
+		$actual = $this->proxy->getDescription();
+        $this->assertEquals($expected, $actual);
+    }
+
+
+
+	/**
+	 * @test
+	 */
+	public function setDescriptionSetsDescription() {
+		$expected = 'desc';
+		$this->proxy->setDescription($expected);
+		$actual = $this->proxy->_get('description');
+		$this->assertEquals($expected, $actual);
+	}
+
+
+
+	/**
+	 * @test
+	 */
+	public function getCreateDateGetsCreateDate() {
+		$expected = '123456789';
+        $this->proxy->_set('createDate', $expected);
+		$actual = $this->proxy->getCreateDate();
+        $this->assertEquals($expected, $actual);
+    }
+
+
+
+	/**
+	 * @test
+	 */
+	public function setCreateDateSetsCreateDate() {
+		$expected = '987654321';
+		$this->proxy->setCreateDate($expected);
+		$actual = $this->proxy->_get('createDate');
+		$this->assertEquals($expected, $actual);
+	}
+
 
 
 	/**
@@ -94,73 +291,27 @@ class Tx_PtExtlist_Tests_Domain_Model_Bookmarks_Bookmark_testcase extends Tx_PtE
 	}
 
 
+
 	/**
 	 * @test
 	 */
-	public function canAccessConstants(){
-		$className = 'Tx_PtExtlist_Domain_Model_Bookmarks_Bookmark';
-		$constantName = 'PTEXTLIST_BOOKMARK_PUBLIC';
-		$expected = 1;
-		$actual = constant(sprintf('%s::%s', $className, $constantName));
+	public function getPidGetsPid() {
+		$expected = '21';
+        $this->proxy->_set('pid', $expected);
+		$actual = $this->proxy->getPid();
+        $this->assertEquals($expected, $actual);
+    }
+
+
+
+	/**
+	 * @test
+	 */
+	public function setPidSetsPid() {
+		$expected = '42';
+		$this->proxy->setPid($expected);
+		$actual = $this->proxy->_get('pid');
 		$this->assertEquals($expected, $actual);
 	}
-    
-    
-//    public function testGetSetCreateDate() {
-//        $bookmark = new Tx_PtExtlist_Domain_Model_Bookmarks_Bookmark();
-//        $bookmark->setCreateDate(1234);
-//        $this->assertEquals($bookmark->getCreateDate(), 1234);
-//    }
-//
-//
-//
-//    public function testGetSetDescription() {
-//        $bookmark = new Tx_PtExtlist_Domain_Model_Bookmarks_Bookmark();
-//        $bookmark->setDescription('description');
-//        $this->assertEquals($bookmark->getDescription(), 'description');
-//    }
-//
-//
-//
-//    public function testGetSetFeUser() {
-//        $feUserMock = $this->getMock('Tx_Extbase_Domain_Model_FrontendUser');
-//        $bookmark = new Tx_PtExtlist_Domain_Model_Bookmarks_Bookmark();
-//        $bookmark->setFeUser($feUserMock);
-//        $this->assertEquals($bookmark->getFeUser(), $feUserMock);
-//    }
-//
-//
-//
-//    public function testGetSetListId() {
-//        $bookmark = new Tx_PtExtlist_Domain_Model_Bookmarks_Bookmark();
-//        $bookmark->setListId('list_id');
-//        $this->assertEquals($bookmark->getListId(),'list_id');
-//    }
-//
-//
-//
-//    public function testGetSetName() {
-//        $bookmark = new Tx_PtExtlist_Domain_Model_Bookmarks_Bookmark();
-//        $bookmark->setName('testname');
-//        $this->assertEquals($bookmark->getName(), 'testname');
-//    }
-//
-//
-//
-//    public function testGetSetFeGroup() {
-//        $bookmark = new Tx_PtExtlist_Domain_Model_Bookmarks_Bookmark();
-//        $feGroupMock = $this->getMock('Tx_Extbase_Domain_Model_FrontendUserGroup');
-//        $bookmark->setFeGroup($feGroupMock);
-//        $this->assertEquals($bookmark->getFeGroup(), $feGroupMock);
-//    }
-//
-//
-//
-//    public function testGetSetIsPublic() {
-//    	$bookmark = new Tx_PtExtlist_Domain_Model_Bookmarks_Bookmark();
-//    	$isPublic = false;
-//    	$bookmark->setIsPublic($isPublic);
-//    	$this->assertEquals($bookmark->getIsPublic(), $isPublic);
-//    }
 
 }
