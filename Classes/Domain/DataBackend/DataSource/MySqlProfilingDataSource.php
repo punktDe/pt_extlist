9be9b0c8795231d12931ab15d71328bc646f3a53
David Vogt <david.h.vogt@gmail.com>
2013-09-17 17:08:20 +0200

    ADD: mysqlDatasource to use for profiling with plumber

---
+++ Classes/Domain/DataBackend/DataSource/MySqlProfilingDataSource.php
@@ -0,0 +1,93 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2010-2011 punkt.de GmbH - Karlsruhe, Germany - http://www.punkt.de
+ *  Authors: Daniel Lienert, Michael Knoll, Christoph Ehscheidt
+ *  All rights reserved
+ *
+ *  For further information: http://extlist.punkt.de <extlist@punkt.de>
+ *
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Class implements data source for mysql databases
+ *
+ * @author Christiane Helmchen
+ * @author David Vogt
+ * @package Domain
+ * @subpackage DataBackend\DataSource
+ */
+class Tx_PtExtlist_Domain_DataBackend_DataSource_MySqlProfilingDataSource extends Tx_PtExtlist_Domain_DataBackend_DataSource_MySqlDataSource {
+
+
+	static private $queryCounter = 1;
+
+
+	/**
+	 * @var array
+	 */
+	protected $preProcessHookObjects;
+
+
+
+	/**
+	 * @var array
+	 */
+	protected $postProcessHookObjects;
+
+
+	public function __construct() {
+		$this->preProcessHookObjects = array();
+		$this->postProcessHookObjects = array();
+		if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_db.php']['queryProcessors'])) {
+			foreach ($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_db.php']['queryProcessors'] as $classRef) {
+				$hookObject = t3lib_div::getUserObj($classRef);
+
+				if (!($hookObject instanceof t3lib_DB_preProcessQueryHook || $hookObject instanceof t3lib_DB_postProcessQueryHook)) {
+					throw new UnexpectedValueException('$hookObject must either implement interface t3lib_DB_preProcessQueryHook or interface t3lib_DB_postProcessQueryHook', 1299158548);
+				}
+				if ($hookObject instanceof t3lib_DB_preProcessQueryHook) {
+					$this->preProcessHookObjects[] = $hookObject;
+				}
+				if ($hookObject instanceof t3lib_DB_postProcessQueryHook) {
+					$this->postProcessHookObjects[] = $hookObject;
+				}
+			}
+		}
+	}
+
+
+
+	public function executeQuery($query) {
+		// Added to log select queries
+		foreach($this->preProcessHookObjects as $preProcessHookObject) { /* @var $preProcessHookObject Tx_SandstormmediaPlumber_Hooks_Hook */
+			$preProcessHookObject->extlist_preProcessAction(self::$queryCounter++ . ' ' . $query);
+		}
+		$result = parent::executeQuery($query);
+
+		// Added to log select queries
+		foreach($this->postProcessHookObjects as $postProcessHookObject) { /* @var $postProcessHookObject Tx_SandstormmediaPlumber_Hooks_Hook */
+			$postProcessHookObject->extlist_postProcessAction();
+		}
+		return $result;
+	}
+
+
+}
