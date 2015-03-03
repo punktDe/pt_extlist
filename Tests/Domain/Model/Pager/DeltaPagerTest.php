c2bc5fccff542908869fb6df56b830b9c89cb521
Michael Lihs <lihs@punkt.de>
2015-03-03 08:01:37 +0100

    [CLEANUP] Remove legacy tests for DeltaPager

--- Tests/Domain/Model/Pager/DeltaPagerTest.php
+++ Tests/Domain/Model/Pager/DeltaPagerTest.php
@@ -166,72 +166,6 @@ class Tx_PtExtlist_Tests_Domain_Model_Pager_DeltaPagerTest extends Tx_PtExtlist_
 
 
 	/**
-	 * Tests for the 'old' DeltaPager to assure that the new class is backwards compatible (concerning tests)
-	 *
-	 * @return array
-	 */
-	public function pagesDataProviderBackwardsCompatibility() {
-
-		return array(
-			'twoPagesAreVisible' => array(
-				'pageCount' => 2, 'delta' => 1, 'firstItemDelta' => 2, 'lastItemDelta' => 2, 'currentPage' => 1,
-				'expectedPages' => array(1=>1, 2=>2)
-			),
-			'threePagesAreVisible' => array(
-				'pageCount' => 3, 'delta' => 1, 'firstItemDelta' => 2, 'lastItemDelta' => 2, 'currentPage' => 1,
-				'expectedPages' => array(1=>1, 2=>2, 3=>3)
-			),
-			'fourPagesAreVisibleAdFirstSelected' => array(
-				'pageCount' => 4, 'delta' => 1, 'firstItemDelta' => 2, 'lastItemDelta' => 2, 'currentPage' => 1,
-				'expectedPages' => array(1=>1, 2=>2, 3=>3, 4=>4)
-			),
-			'fourPagesAreVisibleAndLastSelected' => array(
-				'pageCount' => 4, 'delta' => 1, 'firstItemDelta' => 2, 'lastItemDelta' => 2, 'currentPage' => 4,
-				'expectedPages' => array(1=>1, 2=>2, 3=>3, 4=>4)
-			),
-			'threeFirstPagesAndLastAreVisibleAndFirstPageSelectedAndFivePages' => array(
-				'pageCount' => 5, 'delta' => 1, 'firstItemDelta' => 2, 'lastItemDelta' => 2, 'currentPage' => 1,
-				'expectedPages' => array(1=>1, 2=>2, 3=>3,'bfi'=> '...',5=>5)
-			),
-			'threeFirstPagesAndLastAreVisibleAndThirdPageSelected' => array(
-				'pageCount' => 9, 'delta' => 1, 'firstItemDelta' => 2, 'lastItemDelta' => 2, 'currentPage' => 3,
-				'expectedPages' => array(1=>1, 2=>2, 3=>3, 4 =>4,'bfi'=> '...',9=>9)
-			),
-			'firstAndTheLastThreeAreVisibleWhileLastSelectedAndLessThanTenPages' => array(
-				'pageCount' => 12, 'delta' => 1, 'firstItemDelta' => 2, 'lastItemDelta' => 2, 'currentPage' => 12,
-				'expectedPages' => array(1=>1, 'ffi' => '...', 10=>10, 11 => 11, 12=>12)
-			),
-			'threeFirstPagesAndLastAreVisibleAndFirstPageSelected' => array(
-				'pageCount' => 12, 'delta' => 1, 'firstItemDelta' => 2, 'lastItemDelta' => 2, 'currentPage' => 1,
-				'expectedPages' => array(1=>1, 2=>2, 3=>3, 'bfi'=> '...',12=>12)
-			),
-			'threeFirstPagesAndLastAreVisibleAndSecondPageSelected' => array(
-				'pageCount' => 12, 'delta' => 1, 'firstItemDelta' => 2, 'lastItemDelta' => 2, 'currentPage' => 2,
-				'expectedPages' => array(1=>1, 2=>2, 3=>3, 'bfi'=> '...', 12=>12)
-			),
-			'firstFourAndLastVisibleWhileThirdSelected' => array(
-				'pageCount' => 12, 'delta' => 1, 'firstItemDelta' => 2, 'lastItemDelta' => 2, 'currentPage' => 3,
-				'expectedPages' => array(1=>1, 2=>2, 3=>3, 4 => 4, 'bfi'=> '...', 12=>12)
-			),
-			'firstOneAndLastFourVisibleWhileThirdToLastSelected' => array(
-				'pageCount' => 12, 'delta' => 1, 'firstItemDelta' => 2, 'lastItemDelta' => 2, 'currentPage' => 10,
-				'expectedPages' => array(1=>1, 'ffi' => '...', 9=>9, 10 => 10, 11 => 11, 12=>12)
-			),
-			'firstFourAndLastVisibleWhileFourthSelected' => array(
-				'pageCount' => 12, 'delta' => 1, 'firstItemDelta' => 2, 'lastItemDelta' => 2, 'currentPage' => 4,
-				'expectedPages' => array(1=>1, 'ffi' => '...', 3=>3, 4 => 4,  5=>5, 'bfi'=> '...', 12=>12)
-			),
-			'firstAndTheLastThreeAreVisibleWhileLastSelected' => array(
-				'pageCount' => 12, 'delta' => 1, 'firstItemDelta' => 2, 'lastItemDelta' => 2, 'currentPage' => 12,
-				'expectedPages' => array(1=>1, 'ffi' => '...', 10=>10, 11 => 11, 12=>12)
-			),
-		);
-
-	}
-
-
-
-	/**
 	 * @dataProvider pagesDataProvider
 	 * @test
 	 * @param $pageCount
@@ -242,28 +176,6 @@ class Tx_PtExtlist_Tests_Domain_Model_Pager_DeltaPagerTest extends Tx_PtExtlist_
 	 * @param $expectedPages
 	 */
 	public function getPages($pageCount, $delta, $firstItemDelta, $lastItemDelta, $currentPage, $expectedPages) {
-		$this->pager->_set('totalItemCount', $pageCount);
-		$this->pager->_set('delta', $delta);
-		$this->pager->_set('firstItemDelta', $firstItemDelta);
-		$this->pager->_set('lastItemDelta', $lastItemDelta);
-		$this->pager->_set('currentPage', $currentPage);
-
-		$this->assertEquals($expectedPages, $this->pager->getPages());
-	}
-
-
-
-	/**
-	 * @dataProvider pagesDataProviderBackwardsCompatibility
-	 * @test
-	 * @param $pageCount
-	 * @param $delta
-	 * @param $firstItemDelta
-	 * @param $lastItemDelta
-	 * @param $currentPage
-	 * @param $expectedPages
-	 */
-	public function getPagesBackwardsCompatibility($pageCount, $delta, $firstItemDelta, $lastItemDelta, $currentPage, $expectedPages) {
 		$this->pager->_set('totalItemCount', $pageCount);
 		$this->pager->_set('delta', $delta);
 		$this->pager->_set('firstItemDelta', $firstItemDelta);
