5fbd26000d180aba09cbe32f0c7c17067c635aa1
Daniel Lienert <daniel@lienert.cc>
2015-10-30 15:33:58 +0100

    [TASK] Raise max allowd TYPO3 version to 7.6.*

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '6.2.0-7.0.99',
+      'typo3' => '6.2.0-7.6.99',
       'pt_extbase' => '2.0.0',
     ),
     'conflicts' => 
