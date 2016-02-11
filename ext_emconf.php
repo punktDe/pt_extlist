b6a759987b9e3346b89c2fe89d976b56cb4f6970
Daniel Lienert <daniel@lienert.cc>
2016-02-11 09:59:40 +0100

    [TASK] Bump max TYPO3 Version

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
   array(
     'depends' =>
     array(
-      'typo3' => '6.2.0-7.6.99',
+      'typo3' => '6.2.0-7.9.99',
       'pt_extbase' => '2.1.0',
     ),
     'conflicts' =>
