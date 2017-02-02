ab51c03524268eea230d528eb1f465f763933a39
Christian Herberger <herberger@punkt.de>
2017-02-02 15:07:18 +0100

    TASK: start datepicker late
    jQuery can be ready and the page can be ready but the datepicker script might not be
    ready if it’s loaded by require.js. thus we wait some time. this should be a temporary fix

--- Resources/Private/JavaScriptTemplates/DateRangeDatePicker.js
+++ Resources/Private/JavaScriptTemplates/DateRangeDatePicker.js
@@ -1,25 +1,27 @@
 var $ = $ || TYPO3.jQuery;
 var jQuery = jQuery || TYPO3.jQuery;
 
-$(function() {
-	var baseId = "#typo3-ptextlist-filter-daterangedatepicker-" + "###filterIdentifier###".toLowerCase();
-	var altFormat = "yy-mm-dd";
-	var datePickerFromOptions = {
-		altFormat: altFormat,
-		altField: baseId + "-daterangefrom"
-	};
-	var datePickerToOptions = {
-		altFormat: altFormat,
-		altField: baseId + "-daterangeto"
-	};
+$(function () {
+    window.setTimeout(function () {
+        var baseId = "#typo3-ptextlist-filter-daterangedatepicker-" + "###filterIdentifier###".toLowerCase();
+        var altFormat = "yy-mm-dd";
+        var datePickerFromOptions = {
+            altFormat: altFormat,
+            altField: baseId + "-daterangefrom"
+        };
+        var datePickerToOptions = {
+            altFormat: altFormat,
+            altField: baseId + "-daterangeto"
+        };
 
-	$(baseId + "-from").change(function(){
-		if (!$(this).val()) $(baseId + "-daterangefrom").val('');
-	});
-	$(baseId+ "-to").change(function(){
-		if (!$(this).val()) $(baseId + "-daterangeto").val('');
-	});
+        $(baseId + "-from").change(function () {
+            if (!$(this).val()) $(baseId + "-daterangefrom").val('');
+        });
+        $(baseId + "-to").change(function () {
+            if (!$(this).val()) $(baseId + "-daterangeto").val('');
+        });
 
-	$(baseId + "-from").datepicker(datePickerFromOptions);
-	$(baseId + "-to").datepicker(datePickerToOptions);
+        $(baseId + "-from").datepicker(datePickerFromOptions);
+        $(baseId + "-to").datepicker(datePickerToOptions);
+    }, 1500);
 });
