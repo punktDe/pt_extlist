616535e26122617d9375b91241fbbe6c449ef876
Stefan Köskeroglu <koeskeroglu@punkt.de>
2018-01-16 16:43:39 +0100

    TASK: Fixes a warning error, which is caused by bad parameters

--- Classes/ViewHelpers/Link/SortingViewHelper.php
+++ Classes/ViewHelpers/Link/SortingViewHelper.php
@@ -57,29 +57,41 @@ class  Tx_PtExtlist_ViewHelpers_Link_SortingViewHelper extends \TYPO3\CMS\Fluid\
         $this->sessionPersistenceManagerBuilder = $sessionPersistenceManagerBuilder;
     }
 
+    /**
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('header', 'Tx_PtExtlist_Domain_Model_List_Header_HeaderColumn', 'Header', true);
+    }
 
 
     /**
-     * Renders a link for given header
-     *
-     * @param Tx_PtExtlist_Domain_Model_List_Header_HeaderColumn $header
-     * @param string $action Rendered link for sorting action
-     * @param int $pageUid
-     * @param int $pageType
-     * @param bool $noCache
-     * @param bool $noCacheHash
-     * @param string $section
-     * @param string $format
-     * @param bool $linkAccessRestrictedPages
-     * @param array $additionalParams
-     * @param bool $absolute
-     * @param bool $addQueryString
-     * @param array $argumentsToBeExcludedFromQueryString
+     * @param string $action Target action
+     * @param array $arguments Arguments
+     * @param string $controller Target controller. If NULL current controllerName is used
+     * @param string $extensionName Target Extension Name (without "tx_" prefix and no underscores). If NULL the current extension name is used
+     * @param string $pluginName Target plugin. If empty, the current plugin name is used
+     * @param int $pageUid target page. See TypoLink destination
+     * @param int $pageType type of the target page. See typolink.parameter
+     * @param bool $noCache set this to disable caching for the target page. You should not need this.
+     * @param bool $noCacheHash set this to suppress the cHash query parameter created by TypoLink. You should not need this.
+     * @param string $section the anchor to be added to the URI
+     * @param string $format The requested format, e.g. ".html
+     * @param bool $linkAccessRestrictedPages If set, links pointing to access restricted pages will still link to the page even though the page cannot be accessed.
+     * @param array $additionalParams additional query parameters that won't be prefixed like $arguments (overrule $arguments)
+     * @param bool $absolute If set, the URI of the rendered link is absolute
+     * @param bool $addQueryString If set, the current query parameters will be kept in the URI
+     * @param array $argumentsToBeExcludedFromQueryString arguments to be removed from the URI. Only active if $addQueryString = TRUE
      * @param string $addQueryStringMethod Set which parameters will be kept. Only active if $addQueryString = TRUE
-     * @return string
+     * @return string Rendered link
      */
-    public function render(Tx_PtExtlist_Domain_Model_List_Header_HeaderColumn $header, $action='sort', $pageUid = null, $pageType = 0, $noCache = false, $noCacheHash = false, $section = '', $format = '', $linkAccessRestrictedPages = false, array $additionalParams = [], $absolute = false, $addQueryString = false, array $argumentsToBeExcludedFromQueryString = [], $addQueryStringMethod = null)
+    public function render($action = null, array $arguments = [], $controller = null, $extensionName = null, $pluginName = null, $pageUid = null, $pageType = 0, $noCache = false, $noCacheHash = false, $section = '', $format = '', $linkAccessRestrictedPages = false, array $additionalParams = [], $absolute = false, $addQueryString = false, array $argumentsToBeExcludedFromQueryString = [], $addQueryStringMethod = null)
     {
+        $header = $arguments['header'];
+        if ($action === null) {
+            $action = 'sort';
+        }
         $sortingFieldParams = [];
 
         // We generate sorting parameters for every sorting field configured for this column
